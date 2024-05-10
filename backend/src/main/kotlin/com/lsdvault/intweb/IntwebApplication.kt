cdfd91eee8045eaab46e95bbfd084a2c638fe3de
SaujanShr <saujanshr@gmail.com>
2024-05-10 15:38:01 +0100

    Initialise backend and frontend fw

---
+++ backend/src/main/kotlin/com/lsdvault/intweb/IntwebApplication.kt
@@ -0,0 +1,11 @@
+package com.lsdvault.intweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntwebApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntwebApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/lsdvault/intweb/IntwebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lsdvault.intweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntwebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
